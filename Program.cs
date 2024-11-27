40ed1c23b613e0f47f6d2590cfb5618a5c5439c5
Mahbub-limon <mahbublimon1127@gmail.com>
2024-11-27 15:56:47 +0600

    Frist Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split();
+        long n = long.Parse(inputs[0]);
+        long m = long.Parse(inputs[1]);
+        long a = long.Parse(inputs[2]);
+
+        // Calculating the number of flagstones
+        long flagstonesLength = (n + a - 1) / a;
+        long flagstonesWidth = (m + a - 1) / a;
+
+        // Total flagstones needed
+        long totalFlagstones = flagstonesLength * flagstonesWidth;
+        Console.WriteLine(totalFlagstones);
+    }
+}
+
